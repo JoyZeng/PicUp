d498c23d77a7f1e9d90bdf57b09e4eb89c981578
Joy <joyzeng555@gmail.com>
2018-12-01 19:34:50 -0500

    Add Alamofire

--- PicUp/Controllers/QuotesViewController.swift
+++ PicUp/Controllers/QuotesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import Alamofire
 
 class QuotesViewController: NSViewController {
 
@@ -51,6 +52,20 @@ extension QuotesViewController {
     }
     
     @IBAction func next(sender: NSButton) {
+        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
+        
         currentQuoteIndex = (currentQuoteIndex + 1 + quotes.count) % quotes.count
     }
     
