1c3ec1f594177038907b362800c5d0a217a6a251
Joy <joyzeng555@gmail.com>
2019-02-13 21:50:08 -0500

    Remove useless files

---
+++ PicUp/Controllers/PreferencesWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  PreferencesController.swift
+//  PicUp
+//
+//  Created by Joy on 2019-02-13.
+//  Copyright © 2019 Joy Zeng. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
