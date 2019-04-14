b1f5f8ac280ce791d6e76b96e8ec8dc86fe81cff
Joy <joyzeng555@gmail.com>
2019-04-14 09:45:42 -0400

    Add key

---
+++ PicUp/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  PicUp
+//
+//  Created by Joy on 2018-12-02.
+//  Copyright © 2018 Joy Zeng. All rights reserved.
+//
+
+struct Constants {
+    struct Imgur {
+        static let url = "https://api.imgur.com/3/upload"
+        static let clientId = "738ea37c6b0abea"         // Please respect this key.
+    }
+    
+    struct SMMS {
+        static let url = "https://sm.ms/api/upload"
+        
+    }
+}
